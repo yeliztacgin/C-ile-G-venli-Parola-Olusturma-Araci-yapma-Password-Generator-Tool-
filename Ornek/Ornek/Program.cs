752892e6eeda52e9e44e20f91170d82738fccd7c
yeliztacgin <145569090+yeliztacgin@users.noreply.github.com>
2024-02-18 23:21:30 +0300

    Add files via upload

---
+++ Ornek/Ornek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Guvenli Parolanız Kaç Karakter Olsun");
+            int sayi = int.Parse(Console.ReadLine());
+            string patern = "ABCDEFGHIJKLMNOPSTYVZabcdefghjklpoıuytrew123456789-_?&(%+";
+            Random rastgele = new Random();
+            string createdPass = "";
+            for (int i = 0; i < sayi; i++)
+            {
+                int rastgeleSayi = rastgele.Next(0,patern.Length);
+                createdPass += patern[rastgeleSayi];
+
+            }
+            Console.WriteLine(createdPass);
+            Console.ReadLine();
+        }
+    }
+}
